0dd0b3274611c428c31497d5809953bf3b1ead1f
Ozoezi Chukwujekwu Stanley <43678258+DrStanley@users.noreply.github.com>
2024-05-13 02:24:58 +0100

    100 employes view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container m-2 m-auto">
+    <div class="row row-cols-5">
+      <div class="col mb-2" v-for="(user, index) in users" :key="index">
+        <div class="card p-3 mr-2 ">
+          <img v-bind:src="user.picture" :alt="`${ user.firstName} ${ user.lastName}`" :class=" user.gender" class="m-auto" />
+          <h1>{{  user.irstName }} {{  user.lastName }}</h1>
+          <h3>Gender: {{  user.gender }}</h3>
+          <h3>Email: {{  user.email }}</h3>
+          <!-- <button :disabled="loading" :class="gender" @click="getUser()">Get Random User</button> -->
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    this.getUser();
+  },
+  methods: {
+    async getUser() {
+      this.loading = true;
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=100');
+        const data = await response.json();
+        console.log(data);
+        this.users = await data.results.map(user => ({
+          firstName: user.name.first,
+          lastName: user.name.last,
+          email: user.email,
+          gender: user.gender,
+          picture: user.picture.large,
+        }));
+
+        console.log(users);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      } finally {
+       this.loading = false;
+      }
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
+  box-sizing: border-box;
+  margin: 0;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+html,
+body {
+  font-family: Arial, Helvetica, sans-serif;
 }
-a {
-  color: #42b983;
+
+#app {
+  width: 400px;
+  height: 100vh;
+  margin: auto;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1,
+h3 {
+  margin-bottom: 1rem;
+  font-weight: normal;
+}
+
+img {
+  border-radius: 50%;
+  border: 5px #333 solid;
+  margin-bottom: 1rem;
+  width: 150px;
+  height: 150px;
+}
+
+.male {
+  border-color: steelblue;
+  background-color: steelblue;
+}
+
+.female {
+  border-color: pink;
+  background-color: pink;
+  color: #333;
+}
+
+button {
+  cursor: pointer;
+  display: inline-block;
+  background: #333;
+  color: white;
+  font-size: 18px;
+  border: 0;
+  padding: 1rem 1.5rem;
+}
+
+button:focus {
+  outline: none;
+}
+
+button:disabled {
+  background-color: #727272 !important;
+}
+
+button:hover {
+  transform: scale(0.98);
 }
 </style>
